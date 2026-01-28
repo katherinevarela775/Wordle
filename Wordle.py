a8ce6934fb9e360cbedfefedc950174e7b3ce036
Katherine Varela <katherinevarela775@gmail.com>
2026-01-28 14:41:38 -0300

    Se creo funcion verificador_palabra

---
+++ Wordle.py
@@ -0,0 +1,20 @@
+palabra_secreta = "gordo"
+letras_verificadas = []
+cantidad_letras = 5
+
+
+def verificador_palabra(palabra_ingresada, palabra_secreta):
+    letras_verificadas = []
+
+    for i in range(cantidad_letras):
+        las_palabras_son_iguales = palabra_ingresada[i] == palabra_secreta[i] # True o False
+        la_letra_existe_en_la_palabra = palabra_ingresada[i] in palabra_secreta 
+        
+        if las_palabras_son_iguales:
+            letras_verificadas.append(f"[{palabra_ingresada[i]}]")
+        elif la_letra_existe_en_la_palabra:
+            letras_verificadas.append(f"({palabra_ingresada[i]})")
+        else:
+            letras_verificadas.append(palabra_ingresada[i])
+    
+    return letras_verificadas
